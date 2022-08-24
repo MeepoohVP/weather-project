b54d28626f4def9fa61eb2a206a79541effccbfe
Meepooh VP <90051223+MeepoohVP@users.noreply.github.com>
2022-08-24 18:48:45 +0700

    Reverse

--- WeatherAPI.js
+++ WeatherAPI.js
@@ -15,13 +15,6 @@ const dpImgWeather = document.querySelector("#ImgWeather");
 const dpLocation = document.querySelector("#Location");
 const dpCountry = document.querySelector("#Country");
 const dpDate = document.querySelector("#Date");
-//ForeCase Data
-const fc1ImgWeather = document.querySelector("#ForeCase-1");
-const fc2ImgWeather = document.querySelector("#ForeCase-2");
-const fc1Time = document.querySelector("#ForeCase-1-Time");
-const fc2Time = document.querySelector("#ForeCase-2-Time");
-const fc1Temp = document.querySelector("#ForeCase-1-Temp");
-const fc2Temp = document.querySelector("#ForeCase-2-Temp");
 //data time
 const date = new Date();
 const hour = date.getHours();
@@ -45,6 +38,7 @@ function WeatherAPI() {
     body: '[{"Text":' + beforeTranslating + "}]",
   };
 
+
   //Translating
   fetch(
     "https://microsoft-translator-text.p.rapidapi.com/translate?to%5B0%5D=en&api-version=3.0&profanityAction=NoAction&textType=plain",
@@ -56,9 +50,15 @@ function WeatherAPI() {
 
   //Realtime Weather API
   function dtweather(data) {
-    console.log(data);
-    //data.forecast.forecastday[0].hour[].condition.icon
-    //data.forecast.forecastday[0].hour[].temp_c
+    { // console.log(data);
+      // console.log(data.current.condition.text); //Weather
+      // console.log(data.current.temp_c); //Temp
+      // console.log(data.current.wind_kph);
+      // console.log(data.location.name); //Location
+      // console.log(data.location.country); //Location
+      // console.log(data.location.localtime); //Time
+      // console.log(data.forecast.forecastday[0].day.daily_chance_of_rain); //Rain
+    }
     const Weather = data.current.condition.text;
     const Temp = data.current.temp_c + "&deg;";
     const Wind = data.current.wind_kph;
@@ -66,62 +66,35 @@ function WeatherAPI() {
     const Moisture = data.current.humidity;
     const Location = data.location.name;
     const Country = data.location.country;
-    const lowname = Weather.toLowerCase();
-    const checkweather = ["sunny", "rain", "cloud", "overcast", "mist"]; //mist = หมอก
-    const match = checkweather.map((data) => {
-      return lowname.match(data);
-    });
+
+
+    const lowname = Weather.toLowerCase()
+    const checkweather = ['sunny', 'rain', 'cloud','overcast','mist'];//mist = หมอก
+    const match = checkweather.map(data => {
+      return lowname.match(data)
+    })
+    console.log(match);
     match.forEach((data) => {
       if (data == checkweather[0]) {
         // document.body.style.backgroundColor = 'red';
-        document
-          .getElementById("realtime")
-          .classList.remove(
-            "bg-gradient-to-r",
-            "from-cyan-200",
-            "to-blue-100",
-            "from-gray-400",
-            "to-gray-100"
-          );
-        document
-          .getElementById("realtime")
-          .classList.add(
-            "bg-gradient-to-r",
-            "from-orange-300",
-            "to-orange-100"
-          );
-      } else if (data == checkweather[1] || data == checkweather[3]) {
+        document.getElementById("realtime").classList.remove('bg-gradient-to-r' ,'from-cyan-200' ,'to-blue-100','from-gray-400','to-gray-100');
+        document.getElementById("realtime").classList.add('bg-gradient-to-r' ,'from-orange-300','to-orange-100');
+      }
+      else if (data == checkweather[1] || data == checkweather[3]) {
         // document.body.style.backgroundColor = 'blue';
-        document
-          .getElementById("realtime")
-          .classList.remove(
-            "bg-gradient-to-r",
-            "from-cyan-200",
-            "to-blue-100",
-            "from-orange-300",
-            "to-orange-100"
-          );
-        document
-          .getElementById("realtime")
-          .classList.add("bg-gradient-to-r", "from-gray-400", "to-gray-100");
-      } else if (data == checkweather[2]) {
+        document.getElementById("realtime").classList.remove('bg-gradient-to-r' ,'from-cyan-200' ,'to-blue-100','from-orange-300','to-orange-100');
+        document.getElementById("realtime").classList.add('bg-gradient-to-r' ,'from-gray-400','to-gray-100');
+        
+      }
+      else if (data == checkweather[2] ) {
         // document.body.style.backgroundColor = 'green';
-        document
-          .getElementById("realtime")
-          .classList.remove(
-            "bg-gradient-to-r",
-            "from-gray-400",
-            "to-gray-100",
-            "from-orange-300",
-            "to-orange-100"
-          );
-        document
-          .getElementById("realtime")
-          .classList.add("bg-gradient-to-r", "from-cyan-200", "to-blue-100");
+        document.getElementById("realtime").classList.remove('bg-gradient-to-r' ,'from-gray-400','to-gray-100','from-orange-300','to-orange-100');
+        document.getElementById("realtime").classList.add('bg-gradient-to-r' ,'from-cyan-200' ,'to-blue-100');
         // document.getElementById("realtime").classList.add('from-cyan-200');
         // document.getElementById("realtime").classList.add('to-blue-100');
       }
     });
+
 
     if (data.current.condition.text !== "Clear") {
       dpWeather.innerHTML = Weather;
@@ -131,54 +104,23 @@ function WeatherAPI() {
       dpMoisture.innerHTML = Moisture;
       dpLocation.innerHTML = Location;
       dpCountry.innerHTML = Country;
-      console.log(data);
       setInterval(() => {
         const date = new Date();
-        const hour = date.toLocaleString("en-US", {
-          hour: "numeric",
-          hour12: true,
-        });
-        const hourB = date.getHours();
-        const splithour = hour.split(' ')
-        var hour1 = `${Number(splithour[0]) + 1}:00 ${splithour[1]}`
-        var hour2 = `${Number(splithour[0]) + 2}:00 ${splithour[1]}`
-        const minute = date.getMinutes()
-        const Today = date.getDate() + "/" + (date.getMonth() + 1)
+        const hour = date.getHours();
+        const minute = date.getMinutes();
+        const Today = date.getDate() + "/" + (date.getMonth() + 1);
         const time = date.toLocaleString("en-US", {
           hour: "numeric",
           minute: "numeric",
           hour12: true,
-        })
-        dpTime.innerHTML = time      
-          });
-          if(hourB === 23){
-            hour1 = `${Number(splithour[0]) - Number(splithour[0])}:00 ${splithour[1]}`
-            hour2 = `${(Number(splithour[0]) - Number(splithour[0])) + 1}:00 ${splithour[1]}`
- 
-           fc1Time.innerHTML = hour1
-           fc2Time.innerHTML = hour2
-           fc1Temp.innerHTML = data.forecast.forecastday[0].hour[hourB-hourB].temp_c + '&deg;'
-           fc2Temp.innerHTML = data.forecast.forecastday[0].hour[(hourB-hourB)+1].temp_c + '&deg;'
-           fc1ImgWeather.src = data.forecast.forecastday[0].hour[hourB-hourB].condition.icon
-           fc2ImgWeather.src = data.forecast.forecastday[0].hour[(hourB-hourB)+1].condition.icon
-           
-         }else{
-           if (hourB === 0) {
-             hour1 = `${Number(splithour[0]) - Number(splithour[0])+1}:00 ${splithour[1]}`
-            hour2 = `${(Number(splithour[0]) - Number(splithour[0])) + 2}:00 ${splithour[1]}`
-           }
-           fc1Time.innerHTML = hour1
-           fc2Time.innerHTML = hour2
-           fc1Temp.innerHTML = data.forecast.forecastday[0].hour[hourB+1].temp_c + '&deg;'
-           fc2Temp.innerHTML = data.forecast.forecastday[0].hour[hourB+2].temp_c + '&deg;'
-           fc1ImgWeather.src = data.forecast.forecastday[0].hour[hourB+1].condition.icon
-           fc2ImgWeather.src = data.forecast.forecastday[0].hour[hourB+2].condition.icon
-         }
-          dpRain.innerHTML = data.forecast.forecastday[0].day.daily_chance_of_rain;
-          dpDate.innerHTML = Today;
+        });
+        dpTime.innerHTML = time;
+      })
+      dpTime.innerHTML = time;
+      dpDate.innerHTML = Today;
+      dpRain.innerHTML = data.forecast.forecastday[0].day.daily_chance_of_rain;
     }
   }
-
   setTimeout(() => {
     const option = {
       method: "GET",
@@ -187,7 +129,6 @@ function WeatherAPI() {
         "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
       },
     };
-
     const querydata = Translate.value;
     fetch(
       "https://weatherapi-com.p.rapidapi.com/forecast.json?q=" + querydata,
@@ -196,11 +137,18 @@ function WeatherAPI() {
       .then((response) => response.json())
       .then((data) => dtweather(data))
       .catch((err) => console.error("กรอกข้อมูลที่ถูกต้อง"));
-
     setTimeout(() => {
       DisplayWeather.classList.remove("opacity-0");
       // footer.classList.remove("hidden");
     }, 1000);
   }, 2000);
 }
+
 Info.addEventListener("input", WeatherAPI);
+
+/*สภาพอากาศ 1
+อุณหภูมิ 2
+location 3
+โอกาสที่ฝนจะตก 4
+วันเวลา 4
+ความชื้น 4*/
