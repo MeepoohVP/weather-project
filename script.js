6b68e64ea0df727ee6d60a35e72af3f03c868090
ChuBu0012 <89694571+ChuBu0012@users.noreply.github.com>
2022-08-26 20:42:00 +0700

    animate-realtime-forecase

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const Html = document.querySelector("html");
 const logoBtn = document.querySelector(".ToggleDark");
 const select = document.querySelectorAll(".selection");
-
-
+const Realtime = document.querySelector("#realtime");
+const Forecast_1 = document.querySelector("#Forecast-1-bg");
+const Forecast_2 = document.querySelector("#Forecast-2-bg");
+const selecRealtime = document.querySelector("#selec-realtime");
+const selecForecast = document.querySelector("#selec-forecast");
 
 // System Mode
 {
@@ -21,3 +24,22 @@ const select = document.querySelectorAll(".selection");
   }
   logoBtn.addEventListener("click", ToggleDarkMode);
 }
+
+function togglerealtime(){
+  Realtime.classList.add('animate-spring');
+  setTimeout(() => {
+    Realtime.classList.remove('animate-spring');
+  }, 2000);
+}
+selecRealtime.addEventListener('click',togglerealtime);
+
+function toggleforecast(){
+  Forecast_1.classList.add('animate-springfc-1');
+  Forecast_2.classList.add('animate-springfc-2');
+  setTimeout(() => {
+    Forecast_1.classList.remove('animate-springfc-1');
+    Forecast_2.classList.remove('animate-springfc-2');
+  }, 2000);
+}
+selecForecast.addEventListener('click',toggleforecast)
+
